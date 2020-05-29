0384ad5a4c73579cfcf42ef3af7aef6ddec3c11d
Alessandro Martins <alessandro.f.martins@gmail.com>
2020-05-29 17:12:08 -0300

    Insercao e update na tabela de dados local.

--- webapp/scripts/sleep_journal.js
+++ webapp/scripts/sleep_journal.js
@@ -30,34 +30,57 @@ function millisToFmtTime(tMillis) {
   return (hours > 0 ? hours + 'h' : '') + String(mins % 60).padStart(2, '0') + 'm';
 }
 
+// function generateStats(colName) {
+//   const 
+
+// }
+
 function insertJournalTableRow() {
   let tableRowToInsert = $('<tr></tr>');
   let valToInsert;
+  let insIdx = sleepJournal.entry_date.indexOf($('#entry_date').val());
+
+  if (insIdx == -1)
+    insIdx = sleepJournal.entry_date.length;
+  else {
+    $('#entries-table > tbody')
+      .find(`td:contains("${sleepJournal.entry_date[insIdx]}")`)
+      .parent().remove();
+  }
+
   $('#data-entry').find('.sj-input').toArray().forEach(featureName => {
     valToInsert = $(`#${featureName.id}`).val();
     tableRowToInsert.append($(`<td>${valToInsert}</td>`));
 
-    if (valToInsert.indexOf(':') != -1) {
+    if (valToInsert.indexOf(':') != -1)
       valToInsert = strToDate(valToInsert);
-    }
-    sleepJournal[featureName.id].push(valToInsert);
+    else if (valToInsert.indexOf('/') == -1)
+      valToInsert = parseInt(valToInsert);
+
+    sleepJournal[featureName.id][insIdx] = valToInsert;    
   });
 
-  const lastEntryIdx = sleepJournal.entry_date.length - 1;
-  sleepJournal.total_time_in_bed.push(
-    sleepJournal.time_get_up[lastEntryIdx] - sleepJournal.time_to_bed[lastEntryIdx]
+  sleepJournal.total_time_in_bed[insIdx] =
+    sleepJournal.time_get_up[insIdx] - sleepJournal.time_to_bed[insIdx];
+
+  tableRowToInsert.append(
+    $(`<td>${millisToFmtTime(sleepJournal.total_time_in_bed[insIdx])}</td>`)
+  ); 
+  
+  let actualSleptTime = sleepJournal.total_time_in_bed[insIdx] -
+    ((sleepJournal.delay_to_sleep[insIdx] + sleepJournal.woke_during_night[insIdx]) * 1000 +
+    (sleepJournal.time_get_up[insIdx] - sleepJournal.time_wake_up[insIdx]));
+  
+  sleepJournal.sleep_efficiency.push(
+    actualSleptTime / sleepJournal.total_time_in_bed[insIdx]
   );
-  tableRowToInsert.append($(`<td>${millisToFmtTime(sleepJournal.total_time_in_bed[lastEntryIdx])}</td>`)); 
-  
-  let actualSleptTime = sleepJournal.total_time_in_bed[lastEntryIdx] -
-    ((sleepJournal.delay_to_sleep[lastEntryIdx] + sleepJournal.woke_during_night[lastEntryIdx]) * 1000 +
-    (sleepJournal.time_get_up[lastEntryIdx] - sleepJournal.time_wake_up[lastEntryIdx]));
-  
-  sleepJournal.sleep_efficiency.push(actualSleptTime / sleepJournal.total_time_in_bed[lastEntryIdx]);
-  tableRowToInsert.append($(`<td>${Math.round(10000 * sleepJournal.sleep_efficiency[lastEntryIdx])/100}%</td>`)); 
-  
-  $('#entries-table > tbody').append(tableRowToInsert);
-  window.scroll(0, $('#entries-div').offset().top - 160);
+
+  tableRowToInsert.append(
+    $(`<td>${Math.round(10000 * sleepJournal.sleep_efficiency[insIdx])/100}%</td>`)
+  );
+ 
+  $('#entries-table > tbody').prepend(tableRowToInsert);
+  window.scroll(0, $('#entries-div').offset().top);
 }
 
 
@@ -70,14 +93,9 @@ $(document).ready(function() {
 
   $('#entry_date')
     .val(getToday())
-    .datetimepicker({
-      format:'d/m/Y',
-      timepicker: false
-    });
+    .datetimepicker({format:'d/m/Y', timepicker: false});
 
-    $('#data-entry').find('[id^="time_"]').datetimepicker({
-      format:'d/m/Y H:i'
-    });
+  $('#data-entry').find('[id^="time_"]').datetimepicker({format:'d/m/Y H:i'});
 
   $('#include-data-row').click(function() {
     insertJournalTableRow();
